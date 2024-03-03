782f0942deced135e4b4678fd3cf54673e2b42ef
leomanchic <perleomix@gmail.com>
2024-03-03 19:28:50 +0000

    Update

--- src/main/java/ru/leoman/filesystemsp/service/FileService.java
+++ src/main/java/ru/leoman/filesystemsp/service/FileService.java
@@ -3,12 +3,13 @@ package ru.leoman.filesystemsp.service;
 import lombok.NonNull;
 import ru.leoman.filesystemsp.File;
 
+import java.io.IOException;
 import java.util.List;
 import java.util.UUID;
 
 public interface FileService {
-    void create(@NonNull File file);
-    File read(UUID uuid);
-    List<File> readAll();
-    void delete(UUID uuid);
+    void create(@NonNull File file) throws IOException;
+    File read(UUID uuid) throws IOException;
+    List<File> readAll() throws IOException;
+    void delete(UUID uuid) throws IOException;
 }
