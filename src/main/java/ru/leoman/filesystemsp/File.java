24f75d6e43ada7206bd3f69bfeda4d0d32aab079
leomanchic <perleomix@gmail.com>
2024-03-03 19:30:21 +0000

    Setter annot

--- src/main/java/ru/leoman/filesystemsp/File.java
+++ src/main/java/ru/leoman/filesystemsp/File.java
@@ -1,12 +1,16 @@
 package ru.leoman.filesystemsp;
 
+import com.github.freva.asciitable.AsciiTable;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.UUID;
 @NoArgsConstructor
 @Getter
+@Setter
 public class File {
     private String name;
     private UUID id;
@@ -22,18 +26,6 @@ public class File {
         System.out.println(time_e);
         System.out.println(buffer_size);
     }
-
-    //    Блок сеттеров и геттеров для Названия, Типа файлов, Размер
-//    public String getName(){
-//        return this.name;
-//    }
-//    public String getFile_type(){
-//        return this.file_type;
-//    }
-//    public int getBuffer_size(){
-//        return this.buffer_size;
-//    }
-//    public UUID getId(){return this.id;}
 
     public void setName(String name){
         this.name = name;
@@ -57,6 +49,20 @@ public class File {
                 + this.file_type + "\nSize= " + this.buffer_size + "\nCreated at= " +this.time_c + "\nEdited at= " + this.time_e + "\n}";
     }
 
+    public void TableView(){
+        ArrayList<String[]> table = new ArrayList<>();
+        String[] headers = {"id", "Name", "Created", "Edited", "Type", "Size"};
+        String[] row = {this.id.toString(),this.name, this.time_c.toString(), this.time_e.toString(), this.file_type, Integer.toString(this.buffer_size)};
+        table.add(row);
+        String[][] strings = table.toArray(String[][]::new);
+//        String[][] data = {
+//                {"1", "Mercury", "0.382", "0.06", "minimal"},
+//                {"2", "Venus", "0.949", "0.82", "Carbon dioxide, Nitrogen"},
+//                {"3", "Earth", "1.000", "1.00", "Nitrogen, Oxygen, Argon"},
+//                {"4", "Mars", "0.532", "0.11", "Carbon dioxide, Nitrogen, Argon"}};
+
+        System.out.println(AsciiTable.getTable(headers, strings));
+    }
     File(String name, String file_type, int buffer_size){
         this.name = name;
         id = UUID.randomUUID();
